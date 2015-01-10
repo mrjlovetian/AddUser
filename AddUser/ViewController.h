be23db410b2afcd841388813ac839fb03abfbd8f
mrjlovetian <mrjlovetian@gmail.com>
2015-01-10 14:49:43 +0800

    Initial Commit

---
+++ AddUser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddUser
+//
+//  Created by sbtd on 15/1/10.
+//  Copyright (c) 2015年 sbtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddUserTests/AddUserTests.m
@@ -0,0 +1,40 @@
+//
+//  AddUserTests.m
+//  AddUserTests
+//
+//  Created by sbtd on 15/1/10.
+//  Copyright (c) 2015年 sbtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AddUserTests : XCTestCase
+
+@end
+
+@implementation AddUserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
